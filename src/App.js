8e1eac39ad94eb8172ab408c90a0ffb6d89a392f
Tandy <tandyfw@gmail.com>
2021-02-23 07:51:05 -0300

    todos os inputs text configurados

--- src/App.js
+++ src/App.js
@@ -5,14 +5,32 @@ import Fieldset from './components/Fieldset';
 
 class App extends React.Component{
   render() {
+    const limpar = (target) => {
+      const firstLetter = (target.substring(0,1));
+      if(!isNaN(firstLetter)){
+        return '';
+      }
+      return target;
+    }
+
     return (
       <div className="App">
         <Fieldset legend="Dados Pessoais">
-          <InputText maxLength='40' onChange={ (e) => e.target.value = e.target.value.toUpperCase() }>Nome: </InputText>
-          <InputText maxLength='50'>Email: </InputText>
-          <InputText maxLength='11'>CPF: </InputText>
-          <InputText maxLength='200' onChange={(e) => e.target.value = e.target.value.replace(/[^a-zA-Zs0-9\s,áâãéêíóôõúç]/g, "")}>Endereço: </InputText>
-          <InputText maxLength='28'>Cidade: </InputText>
+          <InputText maxLength='40' onKeyUp={ (e) => e.target.value = e.target.value.toUpperCase() }>
+            Nome: 
+          </InputText>
+          <InputText maxLength='50'>
+            Email: 
+          </InputText>
+          <InputText maxLength='11' onKeyUp={(e) => e.target.value = e.target.value.replace(/[^0-9]/g, "")}>
+            CPF: 
+          </InputText>
+          <InputText maxLength='200' onKeyUp={(e) => e.target.value = e.target.value.replace(/[^a-zA-Zs0-9\s,áâãéêíóôõúç]/g, "")}>
+            Endereço: 
+          </InputText>
+          <InputText maxLength='28' onBlur={(e) => e.target.value = limpar(e.target.value) }>
+            Cidade: 
+          </InputText>
         </Fieldset>
       </div>
     );
--- src/components/InputText.js
+++ src/components/InputText.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class InputText extends React.Component{
   render(){
-    const { children, maxLength, onChange } = this.props;
+    const { children, maxLength, onKeyUp, onBlur } = this.props;
     return(
       <label>{ children }
-        <input type='text' maxLength={ maxLength } onKeyUp={ onChange } pattern="[A-Za-z0-9]" required></input>
+        <input
+          type='text'
+          maxLength={ maxLength }
+          onKeyUp={ onKeyUp }
+          onBlur={ onBlur }
+          required>
+        </input>
       </label>
     )
   }
